b82d994131a7ce3506c6f16784027a29f53bca33
김진호 <russiakim2008@gmail.com>
2024-10-08 22:11:40 +0900

    Atan angle display as text
    Atan angle display as text

--- Assets/01.Scripts/Atan.cs
+++ Assets/01.Scripts/Atan.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Atan : MonoBehaviour
 {
@@ -9,7 +11,7 @@ public class Atan : MonoBehaviour
     private Vector2 _moveDir;
     private float _speed = 5f;
     [SerializeField] private GameObject _enemy;
-    [SerializeField] private GameObject _bullet;
+    [SerializeField] private TextMeshProUGUI _ming;
 
 
     private void Awake()
@@ -29,8 +31,7 @@ public class Atan : MonoBehaviour
         Vector2 distance = _enemy.transform.position - transform.position;
         float z1 = Mathf.Atan(distance.y/distance.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, z1);
-        _bullet.transform.rotation = Quaternion.Euler(0, 0, z1);
-        Debug.Log(z1);
+        _ming.text = z1.ToString();
     }
 
     private void PlayerMove()
@@ -40,5 +41,4 @@ public class Atan : MonoBehaviour
         _moveDir = new Vector2(v, h);
         _ri2.velocity = _moveDir.normalized * _speed;
     }
-    
 }
